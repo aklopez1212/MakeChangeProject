5950bdaaa9a8611750f41b35158afa8dc7fb970f
Alex Lopez <alexander.k.lopez@gmail.com>
2021-05-16 18:35:25 -0600

    Make Change Homework

--- README.md
+++ README.md
@@ -13,5 +13,20 @@ broken down to the denomination which you would be paid! (Example: One Dollar
   - Looping
 
 ### How To Run
-
+Once project is ran you will be prompted for your item total, enter a price.
+Once entered you will then be prompted for cash tendered. Enter an amount.
+After this step is completed the magic happens! Your change will be displayed
+in the console separated by denomination.
 ### Lessons Learned (Pos/Neg)
+Pos +
+I have progressed a lot in terms of combining basic java functions into the same
+program. In the prework I struggled to combine methods when dealing with
+variables and returning values. This exercise has taught me how to do so.
+
+Neg -
+Though the overall project came to me almost naturally (somehow). I did hit a
+roadblock when trying to sysout the change. I was initially working with doubles
+and casting them into integers in my sysout. Which was fine in some cases but
+in others I continually got less pennies or more than I should have. As a
+result I had to pivot and multiply my change by 100 in order to deal with
+strictly integers and the rounding problem I had fixed itself.
--- src/com/skilldistillery/MakeChange.java
+++ src/com/skilldistillery/MakeChange.java
@@ -1,9 +1,81 @@
 package com.skilldistillery;
 
+import java.util.Scanner;
+
 public class MakeChange {
+	static Scanner keyboard = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		int changeInPennies = 0;
 
+		for (int i = 0; i < 10000; i++) {
+
+			welcome();
+			changeInPennies = transaction(changeInPennies);
+
+			if (changeInPennies < 0) {
+				System.err.println("Transaction Failed. Goodbye.");
+			}
+
+			else if (changeInPennies > 0) {
+				int twenties = changeInPennies / 2000;
+				changeInPennies = changeInPennies % 2000;
+				int tens = changeInPennies / 1000;
+				changeInPennies = changeInPennies % 1000;
+				int fives = changeInPennies / 500;
+				changeInPennies = changeInPennies % 500;
+				int ones = (int) changeInPennies / 100;
+				changeInPennies = changeInPennies % 100;
+				int quarters = changeInPennies / 25;
+				changeInPennies = changeInPennies % 25;
+				int dimes = changeInPennies / 10;
+				changeInPennies = changeInPennies % 10;
+				int nickels = changeInPennies / 5;
+				changeInPennies = changeInPennies % 5;
+				int pennies = changeInPennies;
+
+				System.out.println("");
+				System.out.print(twenties + " Twenty + ");
+				System.out.print(tens + " Ten(s) + ");
+				System.out.print(fives + " Five(s) + ");
+				System.out.println(ones + " One(s) + ");
+				System.out.print(quarters + " Quarter(s) + ");
+				System.out.print(dimes + " Dime(s) + ");
+				System.out.print(nickels + " Nickel(s) + ");
+
+				if (pennies < 2) {
+					System.out.println(pennies + " Penny");
+					System.out.println("");
+				} else if (pennies > 2) {
+					System.out.println(pennies + " Pennies");
+					System.out.println("");
+				}
+
+			} else if (changeInPennies == 0) {
+				System.out.println("Have a nice day!");
+			}
+
+		}
+	}
+
+	public static void welcome() {
+// Prints welcome message for user.
+		System.out.println("*****************");
+		System.out.println("*** Welcome To **");
+		System.out.println("*** My Store! ***");
+		System.out.println("*****************");
+		System.out.println("");
+	}
+
+	public static int transaction(int changeInPennies) {
+//This method collects total price of items + cash tendered, returns variable 'changeInPennies'.
+		System.out.println("What is the total price of your items?");
+		double total = keyboard.nextDouble();
+		System.out.print("Cash Tendered = ");
+		double tendered = keyboard.nextDouble();
+		double change = (tendered * 100) - (total * 100);
+		changeInPennies = (int) change;
+		return changeInPennies;
 	}
 
 }
